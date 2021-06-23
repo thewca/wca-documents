34cac20a7c9943c1b9026df12accc284d81675e7
denthebro <alphavitagama@yandex.ru>
2021-06-23 12:40:57 +0900

    Updated builder

--- bin/build.sh
+++ bin/build.sh
@@ -6,12 +6,18 @@ set -x
 wca_url="https://worldcubeassociation.org/"
 stylesheet=$(realpath "./assets/style.css")
 
-# Copy source folders into temporary folders
-cp -r documents/ ./tempdocuments
-cp -r edudoc/ ./tempedudoc
+# Remove potentially cached PDFs from the last build run and make an empty build folder
+rm -rf build
+mkdir build
+# Copy source folders into build
+cp -r documents/ build/
+cp -r edudoc/ build/
+# Copy WCA logo into build/assets temporarily
+mkdir build/assets
+cp assets/WCAlogo_notext.svg build/assets/
 
 # Find formal and legal Markdown files and build PDFs out of them.
-find 'tempdocuments' -name '*.md' | while read file; do
+find 'build/documents' -name '*.md' | while read file; do
   echo "Processing $file, vanilla style"
 
   pdf_name="${file%.md}.pdf"
@@ -38,7 +44,7 @@ compile_date=$(date '+%Y-%m-%d')
 stylesheet=$(realpath "./assets/edudoc-style.css")
 
 # Find educational Markdown files and build PDFs out of them.
-find 'tempedudoc' -name '*.md' | while read file; do
+find 'build/edudoc' -name '*.md' | while read file; do
   echo "Processing $file, edudoc style"
 
   pdf_name="${file%.md}.pdf"
@@ -60,13 +66,6 @@ find 'tempedudoc' -name '*.md' | while read file; do
   wkhtmltopdf --encoding 'utf-8' -T 15mm -B 15mm -R 15mm -L 15mm --header-html "$header_html" --footer-center "[page]" --quiet "$html_name" "$pdf_name"
 done
 
-# Remove potentially cached PDFs from last build run
-rm -rf build
-# Create build directory for working on the conversion of the files
-mkdir build
-# Move temporary folders into build
-mv tempdocuments build/documents
-mv tempedudoc build/edudoc
 # Remove all non-PDF files and empty folders from build
 find build/ -type f -not -name "*.pdf" -delete
 find build/ -type d -empty -delete
