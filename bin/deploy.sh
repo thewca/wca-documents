3e25ec9e4e53aadddafd478127759e3a1c3f5640
Gregor Billing <gregor.billing.dev@gmail.com>
2022-05-06 11:47:33 +0900

    Revert "Migrate deployment script to use pre-packaged server scripts"
    This reverts commit 3e78f366b55f76f99960dbfcea635b999c05d58c.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,4 +4,4 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 /tmp/deploy_rsa # Allow read access to the private key
 ssh-add /tmp/deploy_rsa # Add the private key to SSH
 ssh-keyscan -H $WCA_HOST > $HOME/.ssh/known_hosts # Trust only our own fingerprints
-ssh cubing@$WCA_HOST github-scripts/update-docs.sh
+ssh cubing@$WCA_HOST worldcubeassociation.org/scripts/deploy.sh update_docs
