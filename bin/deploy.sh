709d31af5484d4ec20fa6fa8dd56e6df60d0f821
FinnIckler <finnickler@gmail.com>
2024-01-29 10:28:13 +0100

    Add version to automatically update the monolith

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 aws s3 sync build s3://$DOCUMENTS_BUCKET/ --acl public-read
+# Update version file so the website will update its cache
+current_date=$(date +"%Y-%m-%d %H:%M:%S")
+echo "$current_date" > version
+aws s3 cp version s3://$DOCUMENTS_BUCKET/version
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*" --output text
