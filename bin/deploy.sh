31f4a0e1344516f77dafb565003b7a217abdd925
FinnIckler <finnickler@gmail.com>
2024-01-30 10:58:31 +0100

    Use $GITHUB_SHA for version instead of current date

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,7 +2,7 @@
 
 aws s3 sync build s3://$DOCUMENTS_BUCKET/ --acl public-read
 # Update version file so the website will update its cache
-current_date=$(date +"%Y-%m-%d %H:%M:%S")
-echo "$current_date" > version
+git_hash=$(git rev-parse --short "$GITHUB_SHA")
+echo "$git_hash" > version
 aws s3 cp version s3://$DOCUMENTS_BUCKET/version
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*" --output text
