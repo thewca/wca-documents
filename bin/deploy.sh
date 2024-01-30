26b598924d40e106c1cdcf9908c952371bd2235c
FinnIckler <finnickler@gmail.com>
2024-01-30 12:17:58 +0100

    inline deployment and remove unnecessary step

--- bin/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-aws s3 sync build s3://$DOCUMENTS_BUCKET/ --acl public-read
-# Update version file so the website will update its cache
-git_hash=$(git rev-parse --short "$GITHUB_SHA")
-echo "$git_hash" > version
-aws s3 cp version s3://$DOCUMENTS_BUCKET/version
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*" --output text
