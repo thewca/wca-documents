eadddbc8d44bf6c8cf74f96d38979fc26fcf4911
Deni <epi27314@protonmail.com>
2024-06-15 22:30:11 +0200

    Remove package locking for pandoc and weasyprint

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -5,7 +5,7 @@ if [ -x "$(command -v apt)" ]; then
 
   sudo apt update &&
   # fonts-liberation includes Liberation Sans, which is used in this repo, and fonts-noto is the fallback for wider language support
-  sudo apt install -y wget fonts-liberation fonts-noto pandoc=3.1.3+ds-2 weasyprint=61.1-1
+  sudo apt install -y wget fonts-liberation fonts-noto pandoc weasyprint
 else
   echo "Only Debian-based distros are supported for installing weasyprint. Please install this package manually."
 fi
